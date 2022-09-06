40d751831d448be2a26534b92924cad3807a7ff2
Dmytro <dima0938157970@gmail.com>
2022-09-06 16:11:43 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import AboutUs from "./Components/About_us/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-
+        <AboutUs/>
     </div>
   );
 }
