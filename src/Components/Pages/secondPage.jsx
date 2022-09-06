76f8b86eaa1ff13c6ab808bbab0c5f291ef27b56
Dmytro <dima0938157970@gmail.com>
2022-09-06 19:32:00 +0300

    router

---
+++ src/Components/Pages/secondPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SecondPage = () => {
+    return (
+        <div>
+
+            <h1>Second Page</h1>
+            <p>demo version router 2nd page</p>
+        </div>
+    );
+};
+
+export default SecondPage;
