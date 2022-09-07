467f5b62e89d95d510e25b53f13273d06bf3daa3
Roman Bondar <romanbondar848@gmail.com>
2022-09-07 21:46:55 +0300

    TestRouting

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import { Routes, Route, Link, } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import AboutUs from "./Components/About_us/AboutUs";
-import SecondPage from "./Components/Pages/secondPage";
 import ThirdPage from "./Components/Pages/thirdPage";
 import NotFoundPage from "./Components/Pages/notFoundPage";
 import FourthPage from "./Components/Pages/fourthPage";
 import React from "react";
 
-import MyOffice from './Components/MyOffice/MyOffice'
-
+import MyOffice from './Components/MyOffice/MyOffice';
+import Main from './Components/Main/Main';
 function App() {
   const dispatch = useDispatch()
   const count = useSelector(state => state.products.count)
@@ -17,16 +16,16 @@ function App() {
     <div className="App">
       <header className="App-header">
 
+        <Link className="Link" to="/Main">Main</Link>
         <Link className="Link" to="/MyOffice">MyOffice</Link>
-        <Link className="Link" to="/secondPage">2</Link>
         <Link className="Link" to="/thirdPage">3</Link>
         <Link className="Link" to="/fourthPage">4</Link>
       </header>
 
       <Routes>
         <Route path="/" >home</Route>
+        <Route path="/Main" element={<Main />}></Route>
         <Route path="/MyOffice" element={<MyOffice />}></Route>
-        <Route path="/SecondPage" element={<SecondPage />}></Route>
         <Route path="/ThirdPage" element={<ThirdPage />}></Route>
         <Route path="/fourthPage" element={<FourthPage />}></Route>
         <Route path="*" element={<NotFoundPage />}></Route>
