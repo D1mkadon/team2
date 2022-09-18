5ce7dc0432c813dd5e1600c8049fd21470d8cb0d
Dmytro <dima0938157970@gmail.com>
2022-09-18 17:35:49 +0300

    styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { Routes, Route, Link, } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import AboutUs from "./Components/About_us/AboutUs";
-import ThirdPage from "./Components/Pages/thirdPage";
 import NotFoundPage from "./Components/Pages/notFoundPage";
-import FourthPage from "./Components/Pages/fourthPage";
 import React from "react";
 
 import MyOffice from './Components/MyOffice/MyOffice';
@@ -12,9 +10,13 @@ import Main from './Components/Main/Main';
 import SingleProductPage from "./Components/Main/SingleProductPage/SingleProductPage";
 import ListPage from "./Components/Main/ListPage/ListPage";
 import Cart from './Components/Cart/Cart'
+import FooterDescription from "./Components/FooterDescription/FooterDescription";
+import AboutUsSocial from "./Components/About_us/aboutUsSocial/AboutUsSocial";
 function App() {
   return (
     <div className="App">
+        <div className="wrapper">
+
       <header className="App-header">
 
         <Link className="Link" to="/">Main</Link>
@@ -23,18 +25,23 @@ function App() {
         <Link className="Link" to="/AboutUs">AboutUs</Link>
       </header>
 
-      <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/product/:category/:id" element={<SingleProductPage />}></Route>
-        <Route path="/product/:category" element={<ListPage />}></Route>
-        <Route path="/MyOffice" element={<MyOffice />}></Route>
-        <Route path="/Cart" element={<Cart/>}></Route>
-        <Route path="/AboutUs" element={<AboutUs />}></Route>
-        <Route path="*" element={<NotFoundPage />}></Route>
-      </Routes>
-
-
-
+            <main><div className="Container">
+                <Routes>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route path="/product/:category/:id" element={<SingleProductPage />}></Route>
+                    <Route path="/product/:category" element={<ListPage />}></Route>
+                    <Route path="/MyOffice" element={<MyOffice />}></Route>
+                    <Route path="/Cart" element={<Cart/>}></Route>
+                    <Route path="/AboutUs" element={<AboutUs />}></Route>
+                    <Route path="*" element={<NotFoundPage />}></Route>
+                </Routes>
+            </div>
+            </main>
+            <footer>
+                <FooterDescription/>
+                <AboutUsSocial/>
+            </footer>
+        </div>
 
     </div >
   );
