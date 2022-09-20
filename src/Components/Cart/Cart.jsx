54d9852f1d7ca880aa63cd94dfde09932d824641
Bodjio <body1945@ukr.net>
2022-09-20 19:32:36 +0300

    PriceINCart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './Cart.css'
 import { useDispatch, useSelector } from 'react-redux';
 import Product from '../Main/ListPage/Product/Product';
-import { setInCart, setRemoveItem } from '../reducers/cartReducer';
+import { setAddMoreProducts, setInCart, setRemoveItem } from '../reducers/cartReducer';
 import { Link } from 'react-router-dom';
 
 
@@ -11,28 +11,46 @@ const Cart = () => {
   const arrOfId = useSelector(state => state.cart.arrOfId);
   const nameBtn = useSelector(state => state.cart.inCart);
   const products = useSelector(state=> state.products.item);
- 
+  const totalPrice = useSelector(state=> state.cart.totalPrice);
+  // console.log(arrOfId.indexOf(3));
   const currentCard = products.filter((item) => arrOfId.includes(item.id));
   dispatch(setInCart("Delete"))
- console.log(currentCard);
- function handlerDelete(index){
-  dispatch(setRemoveItem(index))
- }
+
+
   return (
     <div className='CartWrapper'>
         {
+          !totalPrice
+          ?
+          <div className='emptyCart'>
+           <h2> Your cart is empty</h2>
+           <p>But you can to correct it </p>
+            <img src="https://media.istockphoto.com/vectors/empty-shopping-bag-icon-online-business-vector-icon-template-vector-id861576608?k=20&m=861576608&s=612x612&w=0&h=UgHaPYlYrsPTO6BKKTzizGQqFgqEnn7eYK9EOA16uDs=" alt="" />
+            </div>
+          :
             currentCard.map(product =>
                <div>
+                    <div className='totalPrice'>
+                      Total price: {totalPrice.toFixed(2)}
+                    </div>
                     <Link key={product.id} to={`/category/${product.id}`}>
                       <Product prod={product}/>
                     </Link>
                     <button 
                       className='customBtn' 
                       onClick={()=>{
-                        handlerDelete(product.id)
+                        dispatch(setRemoveItem(product.id,product.price))
                       }}
                       >
                         {nameBtn}
+                    </button>
+                    <button 
+                      className='customBtn' 
+                      onClick={()=>{
+                        dispatch(setAddMoreProducts(product.id , product.price))
+                      }}
+                      >
+                        +
                     </button>
                 </div>
             )
--- src/Components/reducers/cartReducer.js
+++ src/Components/reducers/cartReducer.js
@@ -7,10 +7,17 @@ const SET_INCART = "SET_INCART"
 
 const REMOVE_ITEM = "REMOVE_ITEM"
 
+const ADD_TOTAL_PRICE = "ADD_TOTAL_PRICE"
+
+const ADD_MORE_PRODUCT = "ADD_MORE_PRODUCT"
+
+
 
 const defaulStore = {
     arrOfId: [],
     inCart:" Add cart",
+    totalPrice:0,
+    bin:[]
 }
 
 export default function cartReducer(state = defaulStore, action) {
@@ -26,10 +33,22 @@ export default function cartReducer(state = defaulStore, action) {
                 ...state,
                 inCart: action.payload
         }
+        case ADD_TOTAL_PRICE:
+            return {
+                ...state,
+                totalPrice: state.totalPrice += action.payload
+        }
+        case ADD_MORE_PRODUCT:
+            return {
+                ...state,
+                arrOfId: [...state.arrOfId, action.payload],
+                totalPrice: state.totalPrice += action.total
+        }   
         case REMOVE_ITEM:
             return {
                 ...state,
-                arrOfId: state.arrOfId.filter((id) => id !== action.payload)
+                bin: state.arrOfId.splice((state.arrOfId.indexOf(action.payload)),1),
+                totalPrice: state.totalPrice - action.total
         }
 
         default:
@@ -47,9 +66,20 @@ export const setInCart = (inCart) => ({
     payload: inCart
 
 })
+export const setAddTotalPrice = (price) => ({
+    type: ADD_TOTAL_PRICE,
+    payload: price
 
-export const setRemoveItem = (index) => ({
+})
+export const setAddMoreProducts = (idProducta , price) => ({
+    type: ADD_MORE_PRODUCT,
+    payload: idProducta,
+    total: price
+
+})
+export const setRemoveItem = (index , price) => ({
     type: REMOVE_ITEM,
-    payload: index
+    payload: index,
+    total: price,
 
 })
