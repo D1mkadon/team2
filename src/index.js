45fd5a157b2645511c1d2b76c1a9f0eb0452ee6b
Roman Bondar <romanbondar848@gmail.com>
2022-09-21 15:15:10 +0300

    GoogleAuth1

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './Components/reducers/store';
 import { BrowserRouter } from "react-router-dom";
+import {GoogleOAuthProvider} from '@react-oauth/google'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
     <BrowserRouter>
         <Provider store={store}>
-            <App />
+            <GoogleOAuthProvider clientId='443617114822-7pfojtk6iiqj2jkqkae6qbsusjtqchvt.apps.googleusercontent.com'>
+                <App />
+            </GoogleOAuthProvider>
         </Provider>
     </BrowserRouter>
   </React.StrictMode>
